782abcba39cb5153f151b184ecca6c1e54a491e1
BHARGAVRAM GURRAM <73090813+Bhargavram882@users.noreply.github.com>
2025-06-01 16:37:42 -0400

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -13,22 +13,30 @@ export const experienceData = [
         startYear: "February 2023",
         endYear: "July 2023",
     },
-    {
+     {
         id: 5,
+        company: "Coreline solutions",
+        jobtitle: "Machine Learning Intern",
+        startYear: "January 2023",
+        endYear: "May 2023",
+    },
+    
+    {
+        id: 4,
         company: "Artificial Intelligence Institute of South Carolina",
         jobtitle: "Research Intern",
         startYear: "January 2023",
         endYear: "July 2023",
     },
     {
-        id: 4,
+        id: 3,
         company: "Dunzo",
         jobtitle: "Software Engineer - Backend Intern",
         startYear: "January 2023",
         endYear: "June 2023",
     },
     {
-        id: 3,
+        id: 2,
         company: "Innomatics Research Labs",
         jobtitle: "Data Science Intern",
         startYear: "July 2022",
